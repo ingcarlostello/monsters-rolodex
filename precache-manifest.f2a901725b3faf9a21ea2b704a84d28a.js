547038a65fcb07a3ada0470eb22146dafdbfd4af
ingcarlostello <ingecarlostello@gmail.com>
2019-07-29 09:45:34 -0500

    Updates

---
+++ precache-manifest.f2a901725b3faf9a21ea2b704a84d28a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c222d8a8254c35c184733d8c38c23b4a",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "347209330697fb17fb0b",
+    "url": "/monsters-rolodex/static/css/main.d83a86cb.chunk.css"
+  },
+  {
+    "revision": "681b51057d74cfa8975d",
+    "url": "/monsters-rolodex/static/js/2.65aa1cca.chunk.js"
+  },
+  {
+    "revision": "347209330697fb17fb0b",
+    "url": "/monsters-rolodex/static/js/main.eff01c91.chunk.js"
+  },
+  {
+    "revision": "c3f3f5c37da57c0082d3",
+    "url": "/monsters-rolodex/static/js/runtime~main.c2844b1e.js"
+  }
+]);
